adea7fd93156eddfa965fcb4f865d854cb21d26a
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-07 13:54:21 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: A C program that prints wiht printf function
+*
+* Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
