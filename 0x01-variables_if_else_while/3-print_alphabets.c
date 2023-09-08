6ff0c2a59f98b72c9ed2e3db3b1d86bb3d538133
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-08 10:32:46 -0700

    alphabet big n small

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 97;
+	int k = 65;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	while (k <= 90)
+	{
+		putchar(k);
+		k++;
+	}
+
+	putchar('\n');
+	return (0);
+}
