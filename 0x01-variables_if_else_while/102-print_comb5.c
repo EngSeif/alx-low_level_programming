2b0db5a56b8057198d8081bc27110c5fa6e0266b
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-08 15:15:34 -0700

    combination nested complex

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints two combination of digits
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 99)
+	{
+		j = i;
+		while (j <= 99)
+		{
+		if (i != j)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+			putchar((j / 10) + 48);
+			putchar((j / 10) + 48);
+				if (i + j != 197)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+		}
+		j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
