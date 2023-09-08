3710ceb82040abc3f49e8fdb02e6e84ff81c1caa
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-08 10:44:56 -0700

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
