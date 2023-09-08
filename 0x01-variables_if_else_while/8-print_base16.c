82bcfe6df7ba0ded7e0bf2be754cc0563b816335
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-08 11:40:02 -0700

    hexdecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints alphabet big and small
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 48;
+	int k = 97;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	while (k <= 101)
+	{
+		putchar(k);
+		k++;
+	}
+
+	putchar('\n');
+	return (0);
+}
