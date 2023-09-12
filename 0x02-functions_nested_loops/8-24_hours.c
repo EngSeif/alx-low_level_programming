71d66a579ad1f805d4f87ce33f333843627b5943
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 12:51:30 -0700

    clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints clock
+ *
+ * Description: A C program that prints clock
+ *
+ * Return: Always 0 (Success)
+*/
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
