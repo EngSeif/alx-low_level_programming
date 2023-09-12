b72d22450459db8b349cb633d5a83eef5fa7900e
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 10:34:59 -0700

    lowercase check - fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * int _islower - checks for lowercase alphabet
+ * _islower - checks for lowercase alphabet
  *
  * Description: A C program that checks for lowercase alphabet
+ * @c : holds value of charcter ascii
  *
  * Return: Always 0 (Success)
 */
