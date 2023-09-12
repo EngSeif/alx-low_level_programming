77d899c2c67c31378305c6f2270b193633f545db
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 14:27:42 -0700

    n-table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ *
+ * Description: A C program that prints times table
+ *
+ * @n : num
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_times_table(int n)
+{
+	int j;
+	int k;
+
+	while (i < (n + 1))
+	{
+		j = 1;
+		_putchar(48);
+		while (j < (n + 1))
+		{
+			_putchar(',');
+			_putchar(' ');
+			k = i * j;
+			if (k < 10)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((k / 10) + 48);
+			}
+			_putchar((k % 10) + 48);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
