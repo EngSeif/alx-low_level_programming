06c4a95a3e957ed5afde3f39ecb1a1d992ae7cff
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 14:54:24 -0700

    n-table-fix7

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,18 +28,21 @@ void print_times_table(int n)
 			_putchar(' ');
 			_putchar(' ');
 			k = i * j;
-			if (k < 10)
+			if (k <= 9)
 			{
 				_putchar(' ');
 			}
-			else if (k <= 99 && k >= 10)
+			if (k <= 99)
 			{
-				_putchar((k / 10) + 48);
+				_putchar(' ');
 			}
-			else
+			if (prod >= 100)
 			{
 				_putchar((k / 100) + 48);
 				_putchar(((k / 10) % 10) + 48);
+			} else if ((k <= 99 && k >= 10))
+			{
+				_putchar((k / 10) + 48);
 			}
 			_putchar((k % 10) + 48);
 			j++;
