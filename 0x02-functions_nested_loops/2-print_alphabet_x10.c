2aaeba53c6c65c731a7e698d184b0bd3bb362924
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 10:19:30 -0700

    alphabet-10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - use _putchar to print alphabet 10x
+ *
+ * Description: A C program that prints alphabet 10x
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int c;
+	int i=0;
+
+	while ( i < 10 )
+	{
+	for (c = 'a' ; c <= 'z' ; c++)
+		_putchar(c);
+	_putchar('\n');
+	i++;
+	}
+}
