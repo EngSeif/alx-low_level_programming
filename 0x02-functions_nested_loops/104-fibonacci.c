ec5d9ea7df4a2810d3c056fe52639da63ec1dc58
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 15:57:23 -0700

    seq 98 fib sum-fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,20 +11,33 @@
 int main(void)
 {
 	int n = 0;
-	unsigned long f1 = 0, f2 = 1, s;
+	unsigned long f1 = 0, f2 = 1, s, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
 	for (n = 0; n < 98; n++)
 	{
-		s = f1 + f2;
-		printf("%lu", s);
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
 
-		f1 = f2;
-		f2 = s;
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			initial0s--;
+		}
 
-		if (n == 97)
-			printf("\n");
-		else
+		printf("%lu", f1);
+
+		s = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f1;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (n != 98)
 			printf(", ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
