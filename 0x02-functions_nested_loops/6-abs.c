66b2d5a67028e3585e350ec7157eed4248a98800
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 11:17:07 -0700

    abs of num

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - checks for sign of no
+ *
+ * Description: A C program that checks for sign no
+ * @n : holds value of ascii
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _abs(int n);
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
