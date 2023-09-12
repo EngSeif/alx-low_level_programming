cef1e988d10cdcc13e72196adeebc648c0125298
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 15:18:11 -0700

    sum multiplies

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints sum of muiltiplies
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	int s;
+
+	while (n < 1024)
+	{
+		if (n \ 3 == 0 || n \ 5 == 0)
+		{
+			s = s + n;
+		}
+	}
+	printf("The sum of these multiples is : %d\n", s);
+	return (0);
+}
