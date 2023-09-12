e8e81c33bd16f5b5d2390a8fe32c494ac24a4906
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 11:36:19 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints vlast figit
+ *
+ * Description: A C program that prints last digit
+ * @n : holds value of ascii
+ *
+ * Return: Always 0 (Success)
+*/
+
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	return (l);
+}
