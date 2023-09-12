34a6151602cdddb6b534ae7b57e2d5b7d5c0fd75
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-12 14:08:25 -0700

    to-98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print to 98
+ *
+ * Description: A C program that prints to 98
+ *
+ * @n : num
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_to_98(int n);
+{
+	while (n < 99)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+}
