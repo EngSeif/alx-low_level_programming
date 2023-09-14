073b60eee76667f4d73817998ee83b389fd24ef3
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-14 14:33:26 -0700

    digit print 10x fix1

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,17 @@
 */
 void more_numbers(void)
 {
-	int i = 0;
+	int i;
 	int j = 0;
 
 while (j <= 10)
 {
+	i = 0;
 	while (i <= 14)
 	{
 		if (i < 10)
 		{
-		_putchar(i);
+		_putchar(i + 48);
 		}
 		else
 		{
