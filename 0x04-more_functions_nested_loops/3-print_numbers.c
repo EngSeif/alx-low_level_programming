78b0940e2cea7a18649da26b5db27ea7820c9a7c
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-14 14:10:08 -0700

    digit print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ *
+ * Description: A C program that  prints the numbers, from 0 to 9
+ *
+ * Return:(Success)
+*/
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		_putchar('\n');
+		i++;
+	}
+}
