6708e458273211ffefcdc21176ad4bc5c604d181
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-14 13:57:26 -0700

    uppercase fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * isupper -vheck for upercase
+ * _isupper -vheck for upercase
  *
  * Description: A C program that checks for uppercase
+ *
+ * @c : holds ascii value
  *
  * Return:(Success)
 */
 
 int _isupper(int c)
 {
-	if ( c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
