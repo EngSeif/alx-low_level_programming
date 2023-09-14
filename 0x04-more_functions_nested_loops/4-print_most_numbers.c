e919d394c4d642cc1605ef0449b9570b303084bf
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-14 14:19:30 -0700

    digit print most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9 except 4 and 2
+ *
+ * Description: A C program that  prints the numbers, from 0 to 9 except 4 and 2
+ *
+ * Return:(Success)
+*/
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		if (i != 50 && i != 52)
+		{
+		_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
