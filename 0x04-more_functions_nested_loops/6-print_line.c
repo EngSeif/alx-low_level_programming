b7d2636418c6f9eaa68be96fa458a0ed515aa33b
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-14 15:03:08 -0700

    st line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - draws a straight line in the terminal.
+ *
+ * Description: A C program that draws a straight line in the terminal.
+ *
+ * @n : holds line value
+ *
+ * Return:(Success)
+*/
+void print_line(int n)
+{
+	int i
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
