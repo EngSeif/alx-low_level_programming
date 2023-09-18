95fd34721da21a5db6f8781aed18650004045e80
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-18 15:16:13 -0700

    str print other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str : holds string value
+ *
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	int i;
+	
+	for (i = 0; *str != '\0'; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
