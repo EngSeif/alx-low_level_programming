8fbf39a95845a7c4e5d8bd6585f264906287ff4c
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-18 16:21:23 -0700

    str to num fix 1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,6 +21,9 @@ int _atoi(char *s)
 		} else if (*s >= '0' && *s <= '9')
 		{
 			num = (num * 10) + (*s - '0');
+		} else if (num > 0)
+		{
+			break;
 		}
 	} while (*s++);
 	return (num * sign);
