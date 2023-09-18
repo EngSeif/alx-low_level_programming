7d718b11b174276699289d8eb84e39038f39f2fa
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-18 16:31:33 -0700

    gen passwd fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,13 +14,13 @@ int main(void)
 	int sum;
 	char c;
 
-	srand(tume(NULL));
+	srand(time(NULL));
 	while (sum <= 2645)
 	{
 		c = rand() % 128;
 		sum += c;
 		putchar(c);
 	}
-	putchar(2772 - sum)
+	putchar(2772 - sum);
 	return (0);
 }
