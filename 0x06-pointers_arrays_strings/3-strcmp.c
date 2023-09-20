b70e34979a4be69bd0f74626093a6a723b21ded4
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-20 12:36:03 -0700

    str cmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - copies a string.
+ *
+ * @s1 : holds 1st char valu
+ *
+ * @s2 : holds 2nd char value
+ *
+ * Return: void
+*/
+
+int _strcmp(char *s1, char *s2);
+{
+	int i, c;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			c = ((int)*s1 - 48) - ((int)*s2 - 48);
+		}
+	}
+	return (c);
+}
