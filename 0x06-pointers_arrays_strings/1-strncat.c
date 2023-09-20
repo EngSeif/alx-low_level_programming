4ca8ff612739ffa2e20d78dbf43148f283268605
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-20 11:41:27 -0700

    str cat n fix 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,18 +15,15 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int d1;
-	int s2;
 	int i;
 
 	for (d1 = 0; dest[d1] != '\0'; d1++)
 		;
-	for (s2 = 0; src[s2] != '\0'; s2++)
-		;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 	dest[i + d1] = src[i];
 	}
-	dest[i + n] = '\0';
+	dest[i + d1] = '\0';
 
 	return (dest);
 }
