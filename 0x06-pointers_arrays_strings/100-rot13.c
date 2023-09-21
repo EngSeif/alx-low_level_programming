db98a13a65e538740ea9535b73205bbfefd5ae53
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-21 11:01:48 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13.
+ *
+ * @s : holds char values
+ *
+ * Return: void
+*/
+
+char *rot13(char *s)
+{
+	int i;
+
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char Rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int *ptr = s;
+
+	while (*s)
+	{
+		for (i = 0;  i <= 52; i++)
+		{
+			if (*s == rot13[i])
+			{
+				*s = Rot13[i];
+				break;
+			}
+		}
+		c++;
+	}
+
+	return (s);
+}
