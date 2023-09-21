cd7ac63e65109107785818d9c090b5b7202cca0d
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-21 11:14:31 -0700

    print int fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -19,9 +19,9 @@ void print_number(int n)
 		_putchar('-');
 		n1 = -n;
 	}
-	if (n / 10 != 0)
+	if (n1 / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
