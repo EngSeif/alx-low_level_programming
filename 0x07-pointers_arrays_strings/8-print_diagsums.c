9bf0bb8ebca2c606910e341a281a86da3af2c7f5
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-25 15:33:57 -0700

    diagonal sum diagonal fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,13 +10,13 @@
 
 void print_diagsums(int *a, int size)
 {
-int i, s1 = 0, s2 = 0;
+	int i, s1 = 0, s2 = 0;
 
-for (i = 0; i < 3; i++)
-{
-	s1 += a[i];
-	s2 += a[size - i - 1];
-	a += size;
-}
-printf("%d, %d\n", s1, s2);
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, %d\n", s1, s2);
 }
