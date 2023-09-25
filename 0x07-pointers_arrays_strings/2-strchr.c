a56d480bcb024f48bcdfdb7ffc8114052b5e7353
EngSeif <eng.seifmohamed2004@gmail.com>
2023-09-25 12:08:51 -0700

    str chr placex

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ *
+ * @s : holds 1st mem
+ *
+ * @c : holds 2nd mem
+ *
+ * Return: void
+*/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
