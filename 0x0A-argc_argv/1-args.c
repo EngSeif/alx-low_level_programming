ed795564056a832251f9427ea0799fa81bb38d65
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-02 14:08:33 -0700

    N-arg fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,11 +5,12 @@
 *
 * Description: A C program that prints with put function
 * @argc : no
+* @argv : ele
 * * Return: Alawys 0 (Success)
 */
 
-int main(int argc, char **argv __attribute__((unused)) )
+int main(int argc, char **argv __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
