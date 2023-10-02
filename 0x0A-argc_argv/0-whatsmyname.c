ee415fb3be3ca91f3be51fc1766f4b458d166c5a
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-02 13:54:16 -0700

    exe name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: A C program that prints with put function
+* @argv :ele
+* * Return: Alawys 0 (Success)
+*/
+
+int main(char *argv)
+{
+	printff("%s\n", argv[0]);
+}
