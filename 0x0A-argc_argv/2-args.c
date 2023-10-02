ade4906028bbed90aa8b7c59b329c520733bf2a6
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-02 14:13:21 -0700

    print arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: A C program that prints with put function
+* @argc : no
+* @argv : ele
+* * Return: Alawys 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
