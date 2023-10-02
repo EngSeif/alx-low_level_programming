7c5caf2f7ec5b104acad6c31d75f99b8388c0204
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-02 14:48:16 -0700

    add fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,22 +14,16 @@ int main(int argc, char *argv[])
 {
 	int i, sum;
 
-	if (argc == 1)
+
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		if (*argv[i] < '0' || *argv[i] > '9')
 		{
-			if (*argv[i] < '0' || *argv[i] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			sum += atoi(argv[i]);
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", sum);
+	sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
