ca16aae2b95190925106a9388160e6aaff25f522
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-02 14:05:26 -0700

    N-arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: A C program that prints with put function
+* @argc : no
+* * Return: Alawys 0 (Success)
+*/
+
+int main(int argc, char **argv __attribute__((unused)) )
+{
+	printf("%d\n", argc);
+	return (0);
+}
