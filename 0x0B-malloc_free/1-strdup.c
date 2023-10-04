b882e22f17d7336ff2f29e53818fdc139743c217
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-04 08:20:03 -0700

    hello dup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* *_strdup - returns a pointer to a newly allocated space in memor
+* @str : char
+* Return: Alawys 0 (Success)
+*/
+
+char *_strdup(char *str)
+{
+	int i, size = 0;
+	char *array;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (; str[size] != '\0'; size++)
+	array = (char *)malloc((size + 1) * sizeof(*str));
+	if (array == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			array[i] = str[i];
+		}
+	}
+	return (array);
+}
