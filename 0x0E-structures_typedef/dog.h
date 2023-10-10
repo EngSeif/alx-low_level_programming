ce6fb976efdf2844e22bb7996e01d2facf22785a
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-11 00:00:40 +0300

    POPPY

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOGGE_H
+#define DOGGE_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner : third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
