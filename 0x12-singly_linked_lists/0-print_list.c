65c603fd8cd5b0d0a960baa92c26246c904f489e
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-20 01:43:21 +0300

    HotFix 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,20 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t *current = h;
 	size_t count = 0;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str != NULL)
+		if (h->str != NULL)
 		{
-			printf("%s\n", current->str);
+			printf("%s\n", h->str);
 		}
 		else
 		{
 			printf("[0] (nil)\n");
 		}
-		current = current->next;
+		h = h->next;
 		count++;
 	}
 	return (count);
