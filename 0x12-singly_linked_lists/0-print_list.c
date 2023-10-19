6c2cf3dbc5a7c4eb7c7e2ace47bf45b8b7c58408
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-20 02:26:08 +0300

    final bada

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	int i = 0;
+	int s = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)");
-		}
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
+		h = h->next;
+		s++;
 	}
-	return (i);
+	return (s);
 }
