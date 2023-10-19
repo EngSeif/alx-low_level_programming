bde3b6db8f0c0ff6b4fe84a7b5f31d20c4ff1572
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-20 02:22:17 +0300

    final bad

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,22 +2,6 @@
 #include "lists.h"
 
 /**
- * _strlen - gets length of char
- * @s: string
- *
- * Return: the number of chars
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
-/**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
@@ -25,13 +9,18 @@ int _strlen(char *s)
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	int i = 0;
 
 	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-		h = h->next;
-		i++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 	}
 	return (i);
 }
