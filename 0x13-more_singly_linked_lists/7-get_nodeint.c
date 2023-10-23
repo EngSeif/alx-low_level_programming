9b09e9a148521302dc4bc45394b5a0a1d9d5d1f7
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-24 00:27:48 +0300

    Bring The nth | Fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,6 +15,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int c = 0;
 	listint_t *current;
 
+	if (head == NULL)
+		return (NULL);
 	current = head;
 	while (current != NULL)
 	{
