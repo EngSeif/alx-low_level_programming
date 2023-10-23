c37b37bb0ffecc58dd03b57824aa1b044ced798b
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-24 00:22:35 +0300

    Bring The nth

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * get_nodeint_at_index - Free head and return its data
+ * @head: pointer to the listint_t list
+ * @index : position of node to be returned
+ * Return: the number of nodes printed
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int c = 0;
+	listint_t *current;
+
+	current = head;
+	while (current != NULL)
+	{
+		if (c == index)
+		{
+			return (current);
+		}
+		current = head->next;
+		c++;
+	}
+	return (NULL);
+}
