094b9aa7d8a55392cee3fc8225ff5030af65d25c
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-23 23:35:08 +0300

    Free V2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * free_listint2 - Free all of list memory
+ * @head: pointer to the listint_t list
+ *
+ * Return: the number of nodes printed
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	*head = NULL;
+}
