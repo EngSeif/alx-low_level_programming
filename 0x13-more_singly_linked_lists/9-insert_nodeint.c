0df2b27313409319cdb5f49935b5c23f2e61e1e4
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-24 22:30:40 +0300

    Put in nth | Code fix 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,22 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * create_new_node - create a new node
+ * @n : value on data in the new node
+ * Return: the number of nodes printed
+ */
+listint_t *create_new_node(int n)
+{
+	listint_t *new;
 
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	return (new);
+}
 /**
  * insert_nodeint_at_index - sum of data of nodes
  * @head: pointer to the listint_t list
@@ -13,35 +28,38 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new = malloc(sizeof(listint_t));
-
-	listint_t *current;
-
 	unsigned int i;
+	listint_t *tmp, *tmp_old, *new;
 
-	if (new == NULL || head == NULL)
+	tmp = *head;
+	if (head == NULL)
 		return (NULL);
-	new->n = n;
-	new->next = NULL;
+	new = create_new_node(n);
 
-	if (!idx)
+	if (new == NULL)
+		return (NULL);
+
+	if (*head == NULL)
 	{
-		new->next = *head;
 		*head = new;
 		return (new);
 	}
-	current = *head;
-	while (current)
+
+	if (idx == 0)
+		*head = new;
+	for (i = 0; i < idx - 1 && tmp && idx != 0; i++)
 	{
-		if (i == idx - 1)
-		{
-			new->next = current->next;
-			current->next = new;
-			return (new);
-		}
-		i++;
-		current = current->next;
+		tmp = tmp->next;
 	}
-	free(new);
-	return (NULL);
+	if (tmp == NULL)
+		return (NULL);
+	if (idx == 0)
+		new->next = tmp;
+	else
+	{
+		tmp_old = tmp->next;
+		tmp->next = new;
+		new->next = tmp_old;
+	}
+	return (new);
 }
