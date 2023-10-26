5a449e352145a528c0bf4530c81869c755d52074
EngSeif <eng.seifmohamed2004@gmail.com>
2023-10-26 23:38:50 +0200

    Binary value with index | Betty fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_binary - converts binary to unsigned int
- * @n : hold decimal number value
+ * get_bit - get bit at nth index
+ * @n : hold decimal number
+ * @index : nth bit
  * Return: 1 (Success), or -1 (Fail)
  */
 int get_bit(unsigned long int n, unsigned int index)
@@ -14,5 +15,5 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 	}
 
-	return (n & mask) != 0;
+	return (!!(n & mask));
 }
