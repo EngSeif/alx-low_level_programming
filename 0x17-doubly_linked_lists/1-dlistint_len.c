d5e9738edd782842f2a6472c84746f409cf28c2d
EngSeif <eng.seifmohamed2004@gmail.com>
2023-12-07 22:26:23 +0200

    Count

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all elements of the list
+ *
+ * @h: pointer to the head
+ *
+ * Return: size
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		size++;
+	}
+	return (size);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -22,4 +22,5 @@ struct dlistint_s *next;
 
 /* Functions Prototype*/
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 #endif
