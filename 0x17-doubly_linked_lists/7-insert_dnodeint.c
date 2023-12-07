de381afe76f3a1e770c575049541ca3538bd39db
EngSeif <eng.seifmohamed2004@gmail.com>
2023-12-08 01:04:21 +0200

    The ENd | No fix | Sad

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at a certain index
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
  *
- * @h: pointer to the head
- * @idx : index value
- * @n : data value
- * Return: head
-*/
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
@@ -50,3 +51,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	return (new);
+}
