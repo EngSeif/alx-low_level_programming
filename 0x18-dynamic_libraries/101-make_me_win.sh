b84b431ce915288bf39f6cd9fc800d87b7ea7b8f
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-15 22:45:09 +0200

    Mandatory C 0x18| Dynamic Libs

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/aligomaa56/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libdynamic.so
+wget -P /tmp https://github.com/EngSeif/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libdynamic.so
 export LD_PRELOAD=/tmp/libgiga.so
