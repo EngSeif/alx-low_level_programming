e3c731ee8d9f1e5060eb17b51056e91e0d6a8594
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-01 18:01:45 +0200

    Mandatory C 0x1A | Create HashTable | Fix Errors V2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0) /* Check If The Size Is 0 */
 		return (NULL);
 
-	NewHash = malloc(sizeof(hash_node_t));
+	NewHash = malloc(sizeof(hash_table_t));
 
 	if (NewHash == NULL) /* Check If There Is Wrong In Allocation */
 		return (NULL);
