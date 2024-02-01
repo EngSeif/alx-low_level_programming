664d6a11b79644edb8fbf517e6e515cc6f311f68
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-01 21:35:31 +0200

    Mandatory C 0x1A | Print Hash | CodeBeauty Fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,43 +7,43 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i = 0, c1 = 0, c2 = 0;
-    hash_node_t * Tmp;
+	unsigned long int i = 0, c1 = 0, c2 = 0;
+	hash_node_t *Tmp;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        Tmp = ht->array[i];
-        while (Tmp != NULL)
-        {
-            c1++;
-            Tmp = Tmp->next;
-        }
-    }
+	for (i = 0; i < ht->size; i++)
+	{
+		Tmp = ht->array[i];
+		while (Tmp != NULL)
+		{
+			c1++;
+			Tmp = Tmp->next;
+		}
+	}
 
-    if (c1 == 0)
-    {
-        printf("{}\n");
-        return;
-    }
+	if (c1 == 0)
+	{
+		printf("{}\n");
+		return;
+	}
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        Tmp = ht->array[i];
-        while (Tmp != NULL)
-        {
-            c2++;
-            if (c2 < c1)
-                printf("'%s': '%s', ", Tmp->key, Tmp->value);
-            else
-            {
-                printf("'%s': '%s'}\n", Tmp->key, Tmp->value);
-                return;
-            }
-            Tmp = Tmp->next;
-        }
-    }
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		Tmp = ht->array[i];
+		while (Tmp != NULL)
+		{
+			c2++;
+			if (c2 < c1)
+				printf("'%s': '%s', ", Tmp->key, Tmp->value);
+			else
+			{
+				printf("'%s': '%s'}\n", Tmp->key, Tmp->value);
+				return;
+			}
+			Tmp = Tmp->next;
+		}
+	}
 }
